56004461b120ff89acfe4aeabf32c26674083b69
hmaster20 <hmaster20@gmail.com>
2017-01-28 15:48:56 +0300

    Бета версия. Добавлена обработка исключения при запросе

---
+++ Translator/YandexTranslator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace Translator
+{
+    class YandexTranslator
+    {
+        public string Translate(string s, string lang)
+        {
+            if (s.Length > 0)
+            {
+                WebRequest request = WebRequest.Create("https://translate.yandex.net/api/v1.5/tr.json/translate?"
+                    + "key=trnsl.1.1.20170127T184343Z.c57386f5c18826ab.c3942baf91d3a5c948253592772906880173fc24"
+                    + "&text=" + s
+                    + "&lang=" + lang);
+
+                try
+                {
+                    WebResponse response = request.GetResponse();
+
+                    using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+                    {
+                        string line;
+
+                        if ((line = stream.ReadLine()) != null)
+                        {
+                            Translation translation = JsonConvert.DeserializeObject<Translation>(line);
+
+                            s = "";
+
+                            foreach (string str in translation.text)
+                            {
+                                s += str;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.Forms.MessageBox.Show(ex.Message);
+                }
+
+                return s;
+            }
+            else
+                return "";
+        }
+    }
+
+    class Translation
+    {
+        public string code { get; set; }
+        public string lang { get; set; }
+        public string[] text { get; set; }
+    }
+}
